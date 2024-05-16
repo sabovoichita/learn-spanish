339a6760fa8b5b5dba27d6e6ffeafeee81eb29ef
Voichita Maria <sabo_voichita@yahoo.com>
2024-05-16 15:00:12 +0200

    display initial structure  & live-link in Readme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log("Welcome!");
+content = {
+  chapter: "1",
+  title: "Formule de salut",
+  contentR1: ["Bună ziua.", "ceva"],
+  contentS1: ["¡Buenos días!", "altceva"],
+};
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function HTMLContent() {
+  console.log("here");
+  return `<h1>Română - Spaniolă<h1>
+  <h2 class = "here">${content.chapter}. ${content.title}</h2>
+  <p>${content.contentR1.map((a) => a)}</p> <hr/> <p>${content.contentS1}</p>`;
+}
+
+function displayContent() {
+  const place = $("body");
+  //   console.log((place.innerHTML = HTMLContent()));
+  place.innerHTML = HTMLContent();
+}
+
+function initEvents() {
+  displayContent();
+}
+
+initEvents();

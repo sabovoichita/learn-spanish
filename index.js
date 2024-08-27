a5340446cb325183ec8c8b2cbe9948b94acfeb5f
Voichita Maria <sabo_voichita@yahoo.com>
2024-08-27 16:44:27 +0200

    basic new structure

--- index.js
+++ index.js
@@ -1,58 +1,76 @@
-content = {
-  chapter: "1",
-  title: "Formule de salut",
-  contentR1: ["Bună ziua."],
-  contentS1: ["¡Buenos días!"],
-};
-
 function $(selector) {
   return document.querySelector(selector);
 }
 
-function showContent(contents) {
-  var ul = $("body");
-  var contents = ["1", "Formule de salut", "Bună ziua.", "¡Buenos días!"];
-  // console.info(contents);
-  var text = contents.map((content) => {
-    // console.info("inside map %o", content);
-    return `<p>${contents}</p>
+function createStructure() {
+  $("body").innerHTML = `
+  <header>
+    <h1>Learn Spanish<h1>
+  </header>
+  <div id ="container"></div>`;
+}
+
+function renderDivs(lessonNumber) {
+  const container = $("#container");
+  for (let i = 1; i <= lessonNumber; i++) {
+    const lessonDiv = document.createElement("div");
+    lessonDiv.id = `l${i}`;
+    container.appendChild(lessonDiv);
+  }
+}
+
+function showHeader() {
+  return `
+  
+  <div id ="container"></div>
+  `;
+}
+
+function showLesson(lessonNumber, lesson) {
+  const spaceRender = $(`#l${lessonNumber}`);
+  if (!spaceRender) {
+    console.error(`Lesson #l${lessonNumber} not found.`);
+    return;
+  }
+
+  var text = lesson.map((lesson) => {
+    // console.info("inside map %o", lesson);
+    return `
+  <section>
+    <h2 class = "h2Design">Chapter: ${lesson.chapter}. ${lesson.title}</h2>
+    <div class = "displayLesson">
+      <ul>${lesson.lessonS
+        .map((contentItem) => `<li>${contentItem}</li>`)
+        .join(" ")}</li> = ${lesson.lessonE
+      .map((contentItem) => `<li>${contentItem}</li>`)
+      .join(" ")}</ul>
+    </div>
+  </section>
     `;
   });
-  console.warn(text);
-  ul.innerHTML = text.join("");
+  console.warn("lesson is: %o", lessonNumber, text);
+  spaceRender.innerHTML = text.join("");
 }
-showContent();
+// showLesson();
 
-function loadContent() {
-  fetch("./data/1.json").then((r) => {
-    r.json().then((d) => {
-      console.log(d);
-      displayContent(d);
+function loadLesson(lessonNumber) {
+  fetch(`./data/${lessonNumber}.json`).then((r) => {
+    r.json().then((lesson) => {
+      // console.log(lesson);
+      showLesson(lessonNumber, lesson);
     });
   });
 }
 
-function HTMLContent() {
-  return `
-  <header>
-  <h1>Română - Spaniolă<h1>
-  </header>
-  <section>
-  <h2 class = "h2Design">${content.chapter}. ${content.title}</h2>
-  <div class = "displayContent"><p>${content.contentR1}</p> <hr/> <p>${content.contentS1}</p>
-  </div>
-  </section>
-  `;
-}
-
-function displayContent(d) {
-  const place = $("body");
-  place.innerHTML = HTMLContent(d);
-}
+createStructure();
 
 function initEvents() {
-  loadContent();
-  displayContent();
-}
+  const numberOfLessons = 2;
+  renderDivs(numberOfLessons);
 
+  // Load all lessons and insert chapters as needed
+  for (let i = 1; i <= numberOfLessons; i++) {
+    loadLesson(i);
+  }
+}
 initEvents();

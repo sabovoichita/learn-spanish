e952499134ce7508aefb09d572163e2cee0713cd
Voichita Maria <sabo_voichita@yahoo.com>
2024-09-09 18:08:42 +0200

    add lesson 8

--- index.js
+++ index.js
@@ -478,7 +478,7 @@ function setupCollapsibleSections() {
 }
 
 function initEvents() {
-  const numberOfLessons = 7; // Define the number of lessons
+  const numberOfLessons = 8; // Define the number of lessons
   const chapters = []; // Array to hold chapter data
 
   // Fetch chapter titles dynamically
